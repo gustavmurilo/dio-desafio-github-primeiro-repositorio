fc7fee9b92099f69aa53eec87b03996fc939b7c0
Gustavo Murilo <96273534+gustavmurilo@users.noreply.github.com>
2022-01-14 01:30:44 -0300

    ADICIONANDO AULA SOBRE SINTAXE JAVA SCRIPT

---
+++ introducao-ao-java-script/sintaxe-básica-em-java-script/assets/js/script.js
@@ -0,0 +1,103 @@
+// tipos primitvos
+
+// boolean
+var vDuf = false;
+console.log(typeof(vDuf));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nomeQualquer = "Gustavo";
+console.log(typeof(nomeQualquer));
+
+// function
+var function = function() {}
+console.log(typeof(function))
+
+// como declarar com
+var variavel = "Gustavo";
+variavel = "Maria";
+console.log(variavel);
+
+let variavel2 = "josé";
+variavel2 = "Gustavo";
+console.log(variavel2);
+
+const constante = "Gustavo";
+const constante = "Maria";
+console.log(constante)
+
+var escopoGlobal = "Global";
+console.log(escopoGlobal);
+
+function escopoLocal () {
+    let escopoLocalInterno = "local";
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();
+
+// atribuição
+var atribuicao = "Gustavo"
+
+// comparação
+var comparacao = 0 == 0;
+console.log(comparacao)
+
+// comparação identical
+var comparacao = "0" === 0;
+console.log(comparacao)
+
+// adicao
+var adicao = 1 + 2;
+console.log(adicao);
+
+// subtracao
+var subtracao = 1 - 2;
+console.log(subtracao);
+
+// multiplicacao
+var multiplicacao = 1 * 2;
+console.log(multiplicacao);
+
+// divisao real
+var divisao = 1 / 2;
+console.log(divisao);
+
+// divisao inteira
+var divisao = 1 % 2;
+console.log(divisao);
+
+// potenciacao
+var potenciacao = 1 ** 2;
+console.log(potenciacao);
+
+// maior que
+var maiorQue = 19 > 21;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 19 < 21;
+console.log(menorQue);
+
+// maior ou igual
+var maiorOuIgual = 19 >= 21;
+console.log(maiorOuIgual);
+
+// menor ou igual
+var menorOuIgual = 19 >= 21;
+console.log(menorOuIgual); 
+
+// e
+var e = true && true;
+console.log(e);
+
+// ou
+var c = false || false;
+console.log(c);
+
+// não
+var nao = !false;
+console.log(nao);
